1343695de2eda037bafca58840d3c9e7dc83123b
ECEM YAĞIZEL <65720070+ecemygzl@users.noreply.github.com>
2024-05-13 21:42:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,45 +21,55 @@ public:
     void setKonum(float k) { konum = k; }
     float getKonum() const { return konum; }
 
-    bool bitisCizgisineUlastiMi(float bitisKonumu) {
-        if (!bitisCizgisineUlasti && konum >= bitisKonumu) {
+    bool bitisCizgisineUlastiMi(float bitisKonumu) 
+    {
+        if (!bitisCizgisineUlasti && konum >= bitisKonumu) 
+        {
             bitisCizgisineUlasti = true;
             return true;
         }
         return false;
     }
 
-    void parkurGecisi() {
-        if (!parkurGecisiYapildi) {
+    void parkurGecisi() 
+    {
+        if (!parkurGecisiYapildi) 
+        {
             parkurGecisiYapildi = true;
             parkurGecisiZamani = 10; // Parkur geçişinde geçirilecek süre (10 saniye)
 
-            // İkinci parkurdayken hızları 3 katına çıkar
-            if (parkur == 2) {
+            // İkinci parkurda hızlar 3 katına çıkar
+            if (parkur == 2) 
+            {
                 hiz *= 3;
             }
-            // Üçüncü parkurdayken hızları ilk parkurdaki hızlarının 1/3'üne indir
-            else if (parkur == 3) {
+            // Üçüncü parkurda hızlar ilk parkurdaki hızlarının 1/3'üne iner
+            else if (parkur == 3) 
+            {
                 hiz /= 3;
             }
         }
     }
 };
 
-class Takim {
+class Takim 
+{
 private:
     std::vector<Sporcu> sporcular;
 public:
-    void sporcuEkle(const Sporcu& sporcu) {
+    void sporcuEkle(const Sporcu& sporcu) 
+    {
         sporcular.push_back(sporcu);
     }
 
-    const std::vector<Sporcu>& getSpurcular() const {
+    const std::vector<Sporcu>& getSpurcular() const 
+    {
         return sporcular;
     }
 };
 
-class Yaris {
+class Yaris 
+{
 private:
     std::vector<Takim> takimlar;
     int toplamTakimSayisi;
@@ -67,11 +77,14 @@ private:
     int parkur;
 
 public:
-    Yaris(int takimSayisi, int sporcuSayisi) : toplamTakimSayisi(takimSayisi), sporcuSayisi(sporcuSayisi), parkur(1) {
+    Yaris(int takimSayisi, int sporcuSayisi) : toplamTakimSayisi(takimSayisi), sporcuSayisi(sporcuSayisi), parkur(1) 
+    {
         srand(time(NULL));
-        for (int i = 0; i < toplamTakimSayisi; ++i) {
+        for (int i = 0; i < toplamTakimSayisi; ++i) 
+        {
             Takim takim;
-            for (int j = 0; j < sporcuSayisi; ++j) {
+            for (int j = 0; j < sporcuSayisi; ++j) 
+            {
                 float hiz = rand() % 5 + 1; // Rastgele hız (1-5 arası)
                 float konum = 0;
                 Sporcu sporcu;
@@ -83,107 +96,144 @@ public:
         }
     }
 
-    void setSporcuBilgisi(int takimIndex, int sporcuIndex, float hiz, float konum) {
+    void setSporcuBilgisi(int takimIndex, int sporcuIndex, float hiz, float konum) 
+    {
         takimlar[takimIndex].getSpurcular()[sporcuIndex].setHiz(hiz);
         takimlar[takimIndex].getSpurcular()[sporcuIndex].setKonum(konum);
     }
 
-    void yarisBaslat(float bitisKonumu) {
-        std::cout << "Yarış başladı!" << std::endl;
-        while (true) {
-            for (auto& takim : takimlar) {
-                for (auto& sporcu : takim.getSpurcular()) {
-                    if (!sporcu.bitisCizgisineUlastiMi(bitisKonumu)) {
-                        if (!sporcu.parkurGecisiTamamlandiMi()) {
-                            sporcu.parkurGecisiZamaniniAzalt();
-                        } else {
-                            sporcu.parkurGecisi();
-                        }
+    void Yaris::yarisBaslat(float yuzmeMesafesi, float bisikletMesafesi, float kosuMesafesi) 
+    {
+        float bitisKonumu = 0;
+        std::cout << "Yüzme parkuru başladı!" << std::endl;
+        while (bitisKonumu < yuzmeMesafesi) 
+        {
+            // Yüzme parkuru için sporcuların hareketi
+            for (auto& takim : takimlar) 
+            {
+                for (auto& sporcu : takim.getSpurcular()) 
+                {
+                    if (!sporcu.bitisCizgisineUlastiMi(yuzmeMesafesi)) 
+                    {
+                        sporcu.parkurGecisi();
+                        float yeniKonum = sporcu.getKonum() + sporcu.getHiz();
+                        sporcu.setKonum(yeniKonum);
+                    }
+                }
+        
+            }
+            bitisKonumu += yuzmeMesafesi / sporcuSayisi;
+        }
+
+        bitisKonumu = 0;
+        std::cout << "Bisiklet parkuru başladı!" << std::endl;
+        while (bitisKonumu < bisikletMesafesi) 
+        {
+            // Bisiklet parkuru için sporcuların hareketi
+            for (auto& takim : takimlar) 
+            {
+                for (auto& sporcu : takim.getSpurcular()) 
+                {
+                    if (!sporcu.bitisCizgisineUlastiMi(bisikletMesafesi)) 
+                    {
+                        sporcu.parkurGecisi();
                         float yeniKonum = sporcu.getKonum() + sporcu.getHiz();
                         sporcu.setKonum(yeniKonum);
                     }
                 }
             }
-            bool yarisBitti = true;
+            bitisKonumu += bisikletMesafesi / sporcuSayisi;
+        }
+
+        bitisKonumu = 0;
+        std::cout << "Koşu parkuru başladı!" << std::endl;
+        while (bitisKonumu < kosuMesafesi) 
+        {
+            // Koşu parkuru için sporcuların hareketi
             for (auto& takim : takimlar) {
-                for (auto& sporcu : takim.getSpurcular()) {
-                    if (!sporcu.bitisCizgisineUlastiMi(bitisKonumu)) {
-                        yarisBitti = false;
-                        break;
+                for (auto& sporcu : takim.getSpurcular()) 
+                {
+                    if (!sporcu.bitisCizgisineUlastiMi(kosuMesafesi)) 
+                    {
+                        sporcu.parkurGecisi();
+                        float yeniKonum = sporcu.getKonum() + sporcu.getHiz();
+                        sporcu.setKonum(yeniKonum);
                     }
                 }
             }
-            if (yarisBitti) break;
-            parkur++;
+            bitisKonumu += kosuMesafesi / sporcuSayisi;
         }
+    
         std::cout << "Yarış bitti!" << std::endl;
-        takimSirala();
-        derecelendirmeYap();
+            takimSirala();
+            derecelendirmeYap();
     }
 
-    void takimSirala() {
+    void takimSirala() 
+    {
         // Takımların toplam hızlarını hesapla ve sırala
         std::vector<std::pair<int, float>> takimHizlari;
-        for (int i = 0; i < toplamTakimSayisi; ++i) {
+        for (int i = 0; i < toplamTakimSayisi; ++i) 
+        {
             float toplamHiz = 0;
-            for (int j = 0; j < sporcuSayisi; ++j) {
+            for (int j = 0; j < sporcuSayisi; ++j) 
+            {
                 toplamHiz += takimlar[i].getSpurcular()[j].getHiz();
             }
             takimHizlari.push_back(std::make_pair(i, toplamHiz));
         }
-        std::sort(takimHizlari.begin(), takimHizlari.end(), [](const auto& a, const auto& b) {
+        std::sort(takimHizlari.begin(), takimHizlari.end(), [](const auto& a, const auto& b) 
+        {
             return a.second > b.second;
         });
 
         // Takım sıralamasını ekrana yazdır
         std::cout << "Takım Sıralaması:\n";
-        for (int i = 0; i < toplamTakimSayisi; ++i) {
+        for (int i = 0; i < toplamTakimSayisi; ++i) 
+        {
             std::cout << "Takım " << takimHizlari[i].first + 1 << ": " << takimHizlari[i].second << " birim/s\n";
         }
     }
 
-    void derecelendirmeYap() {
+    void derecelendirmeYap() 
+    {
         // Tüm sporcuların derecelerini hesapla ve sırala
         std::vector<std::pair<int, float>> sporcuDereceleri;
-        for (int i = 0; i < toplamTakimSayisi; ++i) {
-            for (int j = 0; j < sporcuSayisi; ++j) {
+        for (int i = 0; i < toplamTakimSayisi; ++i) 
+        {
+            for (int j = 0; j < sporcuSayisi; ++j) 
+            {
                 sporcuDereceleri.push_back(std::make_pair((i * sporcuSayisi) + j, takimlar[i].getSpurcular()[j].getKonum()));
             }
         }
-        std::sort(sporcuDereceleri.begin(), sporcuDereceleri.end(), [](const auto& a, const auto& b) {
+        std::sort(sporcuDereceleri.begin(), sporcuDereceleri.end(), [](const auto& a, const auto& b) 
+        {
             return a.second > b.second;
         });
 
         // Sporcu derecelerini ekrana yazdır
         std::cout << "\nBireysel Dereceler:\n";
-        for (int i = 0; i < toplamTakimSayisi * sporcuSayisi; ++i) {
+        for (int i = 0; i < toplamTakimSayisi * sporcuSayisi; ++i) 
+        {
             std::cout << "Sporcu " << sporcuDereceleri[i].first + 1 << ": " << sporcuDereceleri[i].second << " birim\n";
         }
     }
 };
 
 int main() {
-    int takimSayisi, sporcuSayisi;
-    std::cout << "Takım sayısını giriniz: ";
-    std::cin >> takimSayisi;
-    std::cout << "Her takımdaki sporcu sayısını giriniz: ";
-    std::cin >> sporcuSayisi;
+    int takimSayisi = 300; // Toplam takım sayısı
+    int sporcuSayisi = 3; // Her takımdaki sporcu sayısı
 
     Yaris yaris(takimSayisi, sporcuSayisi);
 
-    // Sporcuların hız ve konum bilgilerini kullanıcıdan al
-    for (int i = 0; i < takimSayisi; ++i) {
-        for (int j = 0; j < sporcuSayisi; ++j) {
-            float hiz, konum;
-            std::cout << "Takım " << i + 1 << ", Sporcu " << j + 1 << " için hız ve konumu giriniz: ";
-            std::cin >> hiz >> konum;
-            yaris.setSporcuBilgisi(i, j, hiz, konum);
-        }
-    }
-
-    // Yarışı başlat ve sonuçları ekrana yazdır
-    float bitisKonumu = 5; // Bitiş çizgisi konumu (örneğin yüzme parkurunun uzunluğu)
+    float bitisKonumu = 5; // Bitiş çizgisi konumu 
     yaris.yarisBaslat(bitisKonumu);
+
+    int sporcuNumarasi;
+    std::cout << "Lutfen bilgi almak istediginiz sporcu numarasini girin: ";
+    std::cin >> sporcuNumarasi;
+
+    yaris.sporcuBilgisiYazdir(sporcuNumarasi);
 
     return 0;
 }
